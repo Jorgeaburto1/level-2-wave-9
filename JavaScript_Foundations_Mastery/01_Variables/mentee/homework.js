1d7ad3ce0e40bac567415dfd4a9aa4ac0a8a712e
Jorge Aburto <jorgeaburto@MacBook-Pro-de-Jorge.local>
2026-03-31 15:39:20 -0600

    i did homework i think

--- JavaScript_Foundations_Mastery/01_Variables/mentee/homework.js
+++ JavaScript_Foundations_Mastery/01_Variables/mentee/homework.js
@@ -6,14 +6,15 @@
 // STEP 1: Create a variable called fullName (string) with your full name.
 const fullName = "jorge Aburto";
 // STEP 2: Create a variable called profileAge (number) with your age.
-
+const profileAge = 25;
 // STEP 3: Create a variable called country (string) with the country you live in.
-
+const profileCountry = ("mexico");
 // STEP 4: Create a variable called favoriteHobby (string) with a hobby you enjoy.
-
+let favoriteHobby = ("surfing");
 // STEP 5: Create a variable called isStudent (boolean) with true or false,
 //         depending on whether you consider yourself a student right now.
-
+const isStundent = true;
+console.log(isStundent)
 // STEP 6: Use console.log to print the profile in a clear format, for example:
 //         --- PROFILE CARD ---
 //         Name: (your name)
@@ -21,7 +22,18 @@ const fullName = "jorge Aburto";
 //         Country: (your country)
 //         Hobby: (your hobby)
 //         Is student? (true/false)
-
+console.log(` Name: ${fullName}\n Age: ${profileAge} \n Country: ${profileCountry} \n Hobby: ${favoriteHobby} \n Is student? ${isStundent}`);
 // STEP 7: Change at least ONE of your variables (for example, favoriteHobby)
 //         to a new value and then log the entire profile card again
 //         to show the updated information.
+
+favoriteHobby = ("Gaming");
+console.log(` Name: ${fullName}\n Age: ${profileAge} \n Country: ${profileCountry} \n Hobby: ${favoriteHobby} \n Is student? ${isStundent}`);
+
+const hobbies = ["Gaming", "Surfing"];
+
+console.log(` Name: ${fullName}\n Age: ${profileAge} \n Country: ${profileCountry} \n Hobby: ${hobbies} \n Is student? ${isStundent}`);
+
+hobbies.push("Working out");
+
+console.log(` Name: ${fullName}\n Age: ${profileAge} \n Country: ${profileCountry} \n Hobby: ${hobbies} \n Is student? ${isStundent}`);
