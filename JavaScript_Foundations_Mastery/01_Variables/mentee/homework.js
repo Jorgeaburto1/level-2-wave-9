53fe6f34b9d627ed22c32f0be1d2e51925c4a195
Jorge Aburto <jorgeaburto@MacBook-Pro-de-Jorge.local>
2026-03-31 23:29:12 -0600

    homework with corrections

--- JavaScript_Foundations_Mastery/01_Variables/mentee/homework.js
+++ JavaScript_Foundations_Mastery/01_Variables/mentee/homework.js
@@ -8,12 +8,12 @@ const fullName = "jorge Aburto";
 // STEP 2: Create a variable called profileAge (number) with your age.
 const profileAge = 25;
 // STEP 3: Create a variable called country (string) with the country you live in.
-const profileCountry = ("mexico");
+const country = ("mexico");
 // STEP 4: Create a variable called favoriteHobby (string) with a hobby you enjoy.
 let favoriteHobby = ("surfing");
 // STEP 5: Create a variable called isStudent (boolean) with true or false,
 //         depending on whether you consider yourself a student right now.
-const isStundent = true;
+const isStudent = true;
 
 // STEP 6: Use console.log to print the profile in a clear format, for example:
 //         --- PROFILE CARD ---
@@ -22,26 +22,31 @@ const isStundent = true;
 //         Country: (your country)
 //         Hobby: (your hobby)
 //         Is student? (true/false)
-console.log(` Name: ${fullName}\n Age: ${profileAge} \n Country: ${profileCountry} \n Hobby: ${favoriteHobby} \n Is student? ${isStundent}`);
+
 // STEP 7: Change at least ONE of your variables (for example, favoriteHobby)
 //         to a new value and then log the entire profile card again
 //         to show the updated information.
 
+console.log(` --- PROFILE CARD --- \n Name: ${fullName}\n Age: ${profileAge} \n Country: ${country} \n Hobby: ${favoriteHobby} \n Is student? ${isStudent}`);
+
+
 favoriteHobby = ("Gaming");
-console.log(` --- PROFILE CARD --- \n Name: ${fullName}\n Age: ${profileAge} \n Country: ${profileCountry} \n Hobby: ${favoriteHobby} \n Is student? ${isStundent}`);
+
+console.log(` --- PROFILE CARD --- \n Name: ${fullName}\n Age: ${profileAge} \n Country: ${country} \n Hobby: ${favoriteHobby} \n Is student? ${isStudent}`);
 
 const hobbies = ["Gaming", "Surfing"];
 
-console.log(` --- PROFILE CARD --- \n Name: ${fullName}\n Age: ${profileAge} \n Country: ${profileCountry} \n Hobby: ${hobbies} \n Is student? ${isStundent}`);
+console.log(` --- PROFILE CARD --- \n Name: ${fullName}\n Age: ${profileAge} \n Country: ${country} \n Hobby: ${hobbies} \n Is student? ${isStudent}`);
 
 hobbies.push("Working out");
 
-console.log(` --- PROFILE CARD --- \n Name: ${fullName}\n Age: ${profileAge} \n Country: ${profileCountry} \n Hobby: ${hobbies} \n Is student? ${isStundent}`);
+console.log(` --- PROFILE CARD --- \n Name: ${fullName}\n Age: ${profileAge} \n Country: ${country} \n Hobby: ${hobbies} \n Is student? ${isStudent}`);
 
 
 console.log(` --- PROFILE CARD --- 
     Name: ${fullName}
     Age: ${profileAge} 
-    Country: ${profileCountry} 
+    Country: ${country} 
     Hobby: ${hobbies} 
-    Is student? ${isStundent}`);
+    Is student? ${isStudent}`);
+
